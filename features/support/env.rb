71cabd8b7e8f739fd86004b797f0da557442272e
nycgui <nyc.gui@gmail.com>
2021-09-24 23:50:38 -0300

    configurando gems necessaria e capybara

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,1 +1,11 @@
-require 'cucumber'
+require 'capybara/cucumber'
+require 'selenium-webdriver'
+require 'site_prism'
+require 'faker'
+require 'pry'
+
+Capybara.configure do |config|
+  config.default_driver = :selenium_chrome
+  config.app_host = 'https://automacaocombatista.herokuapp.com'
+  config.default_max_wait_time = 10
+end
