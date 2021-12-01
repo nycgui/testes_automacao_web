c11baaaade3c6cc15a9e5189fef4f23164b5d130
nycgui <nyc.gui@gmail.com>
2021-11-30 22:50:00 -0300

    adicionando page objects e instanciando

--- features/support/env.rb
+++ features/support/env.rb
@@ -4,6 +4,10 @@ require 'site_prism'
 require 'faker'
 require 'pry'
 
+require_relative 'page_objects'
+
+World(PageObjects)
+
 Capybara.configure do |config|
   config.default_driver = :selenium_chrome
   config.app_host = 'https://automacaocombatista.herokuapp.com'
